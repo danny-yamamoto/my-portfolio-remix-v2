8afc0c10709cd2117db8628e31042ef0bfe1ba0b
danny-yamamoto <yamamoto_daisuke@retail-ai.jp>
2023-10-14 04:13:37 +0000

    Add portfolio

---
+++ app/types.ts
@@ -0,0 +1,26 @@
+export type Experience = {
+    id: string;
+    company: string;
+    position: string;
+}
+
+export type IArticles = {
+    title: string;
+    url: string;
+    id: string;
+};
+
+export type Certificates = {
+    blockchainId: string;
+    title: string;
+}
+
+export type CombinedJson = {
+    myname: string;
+    githubProfile: string;
+    twitterProfile: string;
+    displayExperience: any[];
+    displayArticles: any[];
+    displayRepositories: any[];
+    displayCertificates: any[];
+}
